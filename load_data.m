e0287e71cf2f41ed8cec696a844884ad87aafbda
Dung Truong <dt.young112@gmail.com>
2021-03-02 22:34:18 -0800

    balance subjects

--- load_data.m
+++ load_data.m
@@ -7,7 +7,11 @@ eeglab; close;
 
 folderout = '/expanse/projects/nemar/child-mind-restingstate-dung';
 fileNamesClosed = dir(fullfile(folderout, '*_eyesclosed.set'));
-N = length(fileNamesClosed);
+female = readtable('female.csv');
+female = female.Var1;
+male = readtable('male.csv');
+male = male.Var1;
+N = length(female);
 
 X_train = [];
 Y_train = [];
@@ -21,8 +25,45 @@ Y_test = [];
 N_test_subjs = ceil(N * 0.125);
 N_val_subjs = ceil(N * 0.3125);
 N_train_subjs = length(fileNamesClosed) - N_test_subjs - N_val_subjs;
-for iFile=1:length(fileNamesClosed)
-    EEGeyesc = pop_loadset('filepath', folderout, 'filename', fileNamesClosed(iFile).name);
+for iFile=1:N
+    %% female
+    EEGeyesc = pop_loadset('filepath', folderout, 'filename', [female{iFile} '_eyesclosed.set']);
+    
+    tmpdata = EEGeyesc.data;
+
+    % sub-select channel
+    channel_map = {'Fp1', 22; 'Fp2', 9; 'F7', 33;'F3',24;'Fz', 11;'F4',124;'F8', 122;'FC3', 29;'FCz', 6;'FC4', 111;'T3', 45;'C3', 36;
+        'C4', 104;'T4', 108;'CP3', 42;'CPz', 55;'CP4', 93;'T5', 58;'P3', 52;'Pz', 62;'P4', 92;'T6', 96;'O1', 70; 'Cz', 'Cz'};
+    chanindices = [];
+    for iChannel = 1:size(channel_map,1)
+        if ~ischar(channel_map{iChannel,2})
+            egiChannel = sprintf('E%d', channel_map{iChannel,2});
+            chanindices = [chanindices find(cellfun(@(x) strcmp(x,egiChannel), {EEGeyesc.chanlocs.labels}))];
+        else
+            chanindices = [chanindices find(cellfun(@(x) strcmp(x,'Cz'), {EEGeyesc.chanlocs.labels}))];
+        end
+    end
+    if (length(chanindices) < 24)
+	warning('%s have missing channels. Skipped', fileNamesClosed(iFile).name);
+    	disp(size(tmpdata));
+	continue;
+    end 
+    tmpdata = tmpdata(chanindices,:,:);
+    
+    % append to XOri
+    if (iFile <= N_test_subjs)
+	X_test = cat(3,X_test, tmpdata);
+        Y_test = [Y_test, repelem(EEGeyesc.gender, size(tmpdata,3))];
+    elseif (iFile <= N_test_subjs + N_val_subjs)
+	X_val = cat(3, X_val, tmpdata);
+        Y_val = [Y_val, repelem(EEGeyesc.gender, size(tmpdata,3))];
+    else
+	X_train = cat(3, X_train,tmpdata);
+    Y_train = [Y_train, repelem(EEGeyesc.gender, size(tmpdata,3))];
+    end
+    
+    %% male
+    EEGeyesc = pop_loadset('filepath', folderout, 'filename', [male{iFile} '_eyesclosed.set']);
     
     tmpdata = EEGeyesc.data;
 
