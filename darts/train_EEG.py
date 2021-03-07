b8f50edb48a388366b116c50d85c03ea4f7c4b1b
Dung Truong <dt.young112@gmail.com>
2021-03-07 13:52:22 -0800

    fix item(0) bug

--- darts/train_EEG.py
+++ darts/train_EEG.py
@@ -157,7 +157,7 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
     #prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
     prec1 = utils.accuracy(logits, target)
     objs.update(loss.data, n)
-    top1.update(prec1, n)
+    top1.update(prec1[0], n)
     # top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
